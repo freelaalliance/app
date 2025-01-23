00d2e57245d7b114982327bdf01e5316dae9b1cc
Adriano Silva <adrsilva613@gmail.com>
2025-01-23 08:59:23 -0300

    Correção ESLINT e componente Calendar

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,59 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
 export default [
-    ...compat.extends("next/core-web-vitals", "@rocketseat/eslint-config/next"),
-    {
-        rules: {
-            "prefer-arrow-callback": "error",
+  ...compat.extends('next/core-web-vitals', '@rocketseat/eslint-config/next'),
+  {
+    rules: {
+      'prefer-arrow-callback': 'error',
 
-            "no-console": ["warn", {
-                allow: ["warn", "error"],
-            }],
-
-            "sort-imports": ["error", {
-                ignoreCase: true,
-                ignoreDeclarationSort: true,
-                ignoreMemberSort: false,
-                memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
-            }],
-
-            "import/order": ["error", {
-                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-                "newlines-between": "always",
-
-                alphabetize: {
-                    order: "asc",
-                    caseInsensitive: true,
-                },
-            }],
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
         },
+      ],
+
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        },
+      ],
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
-];
+  },
+]
