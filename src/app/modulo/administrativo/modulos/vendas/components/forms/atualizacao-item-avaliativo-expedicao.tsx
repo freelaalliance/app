62b86d625e85c2de4d6941f63137355af5dea0bb
Adriano Silva <adrsilva613@gmail.com.br>
2025-06-16 10:38:37 -0300

    fix: corrigir tipo do campo id para number no schema e na interface do formulário

--- src/app/modulo/administrativo/modulos/vendas/components/forms/atualizacao-item-avaliativo-expedicao.tsx
+++ src/app/modulo/administrativo/modulos/vendas/components/forms/atualizacao-item-avaliativo-expedicao.tsx
@@ -13,7 +13,7 @@ import { z } from 'zod'
 import { atualizarDescricaoItemAvaliativosExpedicao } from '../../../_api/AdmVendas'
 
 const schemaAtualizacaoItemAvaliativoExpedicao = z.object({
-  id: z.string().uuid(),
+  id: z.coerce.number(),
   pergunta: z.string({
     required_error: 'Obrigatório adicionar a descrição do item',
   }),
@@ -24,7 +24,7 @@ export type ItemAvaliativoExpedicaoFormType = z.infer<
 >
 
 export interface AtualizacaoItemAvaliativoExpedicaoFormProps {
-  id: string
+  id: number
   pergunta: string
 }
 
