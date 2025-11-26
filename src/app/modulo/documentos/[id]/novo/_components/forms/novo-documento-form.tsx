17f02e7086206b404f747e6322c491b6ca4831bd
Adriano Silva <adrsilva613@gmail.com.br>
2025-11-26 19:39:47 -0300

    fix: ajustar valor mínimo do campo Número da revisão para 0 nos formulários de documentos

--- src/app/modulo/documentos/[id]/novo/_components/forms/novo-documento-form.tsx
+++ src/app/modulo/documentos/[id]/novo/_components/forms/novo-documento-form.tsx
@@ -494,7 +494,7 @@ export function NovoDocumentoForm({
                 <FormItem>
                   <FormLabel>Número da revisão</FormLabel>
                   <FormControl>
-                    <Input type="number" {...field} min={1} />
+                    <Input type="number" {...field} min={0} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
