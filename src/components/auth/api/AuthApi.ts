bf9c026682d8678dc0b0cd3c6f00a2e32d1f8f8a
Adriano Silva <adrsilva613@gmail.com.br>
2026-02-18 10:51:53 -0300

    feat: adicionar interface AuthResponse e refatorar função logout

--- src/components/auth/api/AuthApi.ts
+++ src/components/auth/api/AuthApi.ts
@@ -4,6 +4,11 @@ import type {
 } from '@/components/auth/schema/SchemaUsuario'
 import { axiosInstance } from '@/lib/AxiosLib'
 
+export interface AuthResponse {
+  status: boolean
+  msg: string
+}
+
 export async function recuperarDadosUsuarioSessao() {
   const response = await axiosInstance.get<UsuarioType>('usuario/dados')
 
@@ -17,4 +22,16 @@ export async function login({ email, senha }: AuthPropsInterface) {
   })
 
   return response.data
+}
+
+export async function logout(): Promise<AuthResponse> {
+  try {
+    const { data } = await axiosInstance.post<AuthResponse>('/usuario/logout')
+
+    return data
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  } catch (error: any) {
+    const errorMessage = error.response?.data?.msg || 'Erro ao fazer logout'
+    throw new Error(errorMessage)
+  }
 }
--- src/components/auth/api/SessaoUsuario.ts
+++ src/components/auth/api/SessaoUsuario.ts
@@ -2,8 +2,10 @@
 
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { logout } from './AuthApi'
 
 export async function closeSession() {
+  await logout()
   cookies().delete('sessionUser')
   redirect('/')
 }
