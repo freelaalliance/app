635a270479403389f0e83edec8a9fa6f98260d1e
Adriano Silva <adrsilva613@gmail.com.br>
2026-03-03 22:58:14 -0300

    fix: corrigir rota de visualização do pedido para incluir o caminho do fornecedor

--- src/app/modulo/compras/[id]/fornecedor/(views)/NovoPedido.tsx
+++ src/app/modulo/compras/[id]/fornecedor/(views)/NovoPedido.tsx
@@ -163,7 +163,7 @@ export default function NovoPedidoView({ fornecedorId }: NovoPedidoProps) {
           action: {
             onClick: () => {
               router.push(
-                `pedido/${data.dados?.id}/visualizar?codigo=${data.dados?.codigo}`
+                `fornecedor/pedido/${data.dados?.id}/visualizar?codigo=${data.dados?.codigo}`
               )
             },
             label: 'Visualizar pedido',
